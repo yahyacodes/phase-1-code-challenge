6342a391398a67652ad1805dde405bcddfc0dad5
yahyacodes <yahya.codin@gmail.com>
2023-09-29 22:08:34 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,52 @@
-// Your code here
+const character = document.querySelector("#character-bar");
+
+fetch("http://localhost:3000/characters")
+  .then((res) => res.json())
+  .then((data) => {
+    const animalNames = document.querySelector("div#character-bar");
+    data.forEach((animal) => {
+      const cuteAnimal = animal.name;
+      const spanTag = document.createElement("span");
+      const animalList = document.createElement("ul");
+      spanTag.innerHTML = cuteAnimal;
+      animalNames.appendChild(spanTag);
+      spanTag.appendChild(animalList);
+      spanTag.addEventListener("click", () => {
+        characterDetails(animal.id);
+      });
+    });
+  });
+
+const characterDetails = (id) => {
+  const detailedInfo = document.querySelector("div#detailed-info");
+  detailedInfo.innerHTML = "";
+  fetch(`http://localhost:3000/characters/${id}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const animalName = document.createElement("h2");
+      animalName.innerHTML = data.name;
+      detailedInfo.appendChild(animalName);
+
+      const animalImage = document.createElement("img");
+      animalImage.src = data.image;
+      animalImage.alt = data.name;
+      detailedInfo.appendChild(animalImage);
+
+      const animalVotes = document.createElement("p");
+      animalVotes.innerHTML = data.votes;
+      detailedInfo.appendChild(animalVotes);
+    });
+};
+
+const updateVotes = () => {
+  const votesInput = document.querySelector("#votes");
+  const formVotes = document.querySelector("form#votes-form");
+  fetch(`http://localhost:3000/characters/`)
+    .then((res) => res.json())
+    .then((data) => {
+      formVotes.addEventListener("submit", (e) => {
+        e.preventDefault();
+        data.votes = votesInput.value;
+      });
+    });
+};
